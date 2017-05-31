a371feafa9c809c8f2d0563c3193d7f9f5fc688b
jackthias <jackthias@pummelhead.com>
2017-05-31 15:00:04 -0700

    Added comment headers to all scripts, as well as a brief statement citing the tutorial referenced in the creation of these scripts.

--- Barbecue Bash/Assets/Scripts/Food.cs
+++ Barbecue Bash/Assets/Scripts/Food.cs
@@ -1,3 +1,11 @@
+//Barbecue Bash Team
+//May 24th, 2017
+//Food.cs
+//Handles an individual tile.
+//Designed Based on the Tutorial: "Building a match-3 game (like Candy Crush) in Unity"
+//via Dimitris-Ilias Gkanatsios
+//URL: https://dgkanatsios.com/2015/02/25/building-a-match-3-game-in-unity-3/
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
--- Barbecue Bash/Assets/Scripts/GameBoard.cs
+++ Barbecue Bash/Assets/Scripts/GameBoard.cs
@@ -1,3 +1,11 @@
+//Barbecue Bash Team
+//May 24th, 2017
+//GameBoard.cs
+//Handles the playing board and the tiles on it.
+//Designed Based on the Tutorial: "Building a match-3 game (like Candy Crush) in Unity"
+//via Dimitris-Ilias Gkanatsios
+//URL: https://dgkanatsios.com/2015/02/25/building-a-match-3-game-in-unity-3/
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
